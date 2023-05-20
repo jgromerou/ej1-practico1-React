31c74b00d77163f158121e68bb976fadcc616d2e
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-05-20 14:35:07 -0300

    Ejercicio1 terminado del Practico 1 de React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="fw-bold display-3">Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
